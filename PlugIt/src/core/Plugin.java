26b12e80dee46b5b2f923bcd9323887fdc6fb14f
danielgalassi@gmail.com <danielgalassi@gmail.com@ac74f605-dd49-9f55-c07e-55e3b2bb1f51>
2011-02-25 23:40:51 +0000

    New method (getResNotes)

--- PlugIt/src/core/Plugin.java
+++ PlugIt/src/core/Plugin.java
@@ -12,6 +12,7 @@ public interface Plugin {
 	public boolean isReady();
 	public String getName();
 	public String getDescription();
+	public String getResNotes();
 	public String getStatus();
 	public boolean passed();
 	public void reset();
